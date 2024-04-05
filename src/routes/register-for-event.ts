e745074cd773fb46b1988fd6acf8508a3b8d49ff
FellipeMiguel <fellipemiguel1@icloud.com>
2024-04-05 12:49:31 -0300

    create  attendees table and database relationship

---
+++ src/routes/register-for-event.ts
@@ -0,0 +1,6 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+
+export function registerForEvent(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>();
+}
